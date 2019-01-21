7d8cf0b01139fdd6fc1c99229d1ecb1f5e2936cd
Evgenii Starostin <e.starostin89@gmail.com>
2019-01-21 21:00:43 +0300

    sets up infrastructure

---
+++ api/app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/attendancedb', { useNewUrlParser: true });
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/api', indexRouter);
+app.use('/api/users', usersRouter);
+
+module.exports = app;
